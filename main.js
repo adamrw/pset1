e7b081201dbd82e7ab3f9c0bfc4a7d8ff894c953
Adam Weiner <adamwineguy@gmail.com>
2014-09-18 12:44:23 -0600

    finished problem set 1

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-$(document).on('ready', function() {
-  
-});
+// capitolize first letter
+
+// var letterCapitolize=function(words) {
+// 	return words.charAt(0).toUpperCase();
+// };
+// letterCapitolize('this');
+// letterCapitolize('is');
+// letterCapitolize('a');
+// letterCapitolize('test');
+
+
+// count words
+
+var wordCount=function(words) {
+	console.log(words.split(' '));
+	return words.split(' ').length;
+};
+
+console.log(wordCount('this is a test'));
+
